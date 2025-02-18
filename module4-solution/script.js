f7d9b6ad503ed9bfe5d216f89a8ffe1fd5067e5f
Cristina Irene Fortiz <cfortiz1@jhu.edu>
2025-02-18 06:29:32 -0700

    adding in the solution

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,13 +34,16 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // STEP 10:
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
+  // See Lecture 50, part 1
+  // Part 1: Loop over names and print greetings
+  for (var i = 0; i < names.length; i++) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -49,16 +52,41 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i]);
   }
 }
+
+  function determineGreeting(name) {
+    var firstLetter = name.charAt(0).toLowerCase();
+    return (firstLetter === 'j') ? byeSpeaker.speakSimple(name) : helloSpeaker.speakSimple(name);
+  }
+
+  var greetingsArray = names.map(determineGreeting);
+  console.log("\nUsing map():");
+  console.log(greetingsArray);
+
+  var groupedGreetings = names.reduce((acc, name) => {
+    var firstLetter = name.charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      acc.bye.push(byeSpeaker.speakSimple(name));
+    } else {
+      acc.hello.push(helloSpeaker.speakSimple(name));
+    }
+    return acc;
+  }, { hello: [], bye: [] });
+
+  console.log("\nUsing reduce():");
+  console.log("Hello Group:", groupedGreetings.hello);
+  console.log("Goodbye Group:", groupedGreetings.bye);
+})();
