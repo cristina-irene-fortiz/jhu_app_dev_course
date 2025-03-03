ecfcf342a51e9933b782720de42b95ad9dd27394
Cristina Irene Fortiz <cfortiz1@jhu.edu>
2025-03-03 14:37:46 -0700

    added gifs

--- module6-solution/js/app.js
+++ module6-solution/js/app.js
@@ -9,11 +9,13 @@
         $scope.lunchItems = "";
         $scope.message = "";
         $scope.messageStyle = {};
+        $scope.gifUrl = "";  
 
         $scope.checkLunch = function () {
             if (!$scope.lunchItems.trim()) {
                 $scope.message = "Please enter data first";
                 $scope.messageStyle = { "color": "red" };
+                $scope.gifUrl = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTA3M2ltcmNud2RneW82dzliODQ1anZtMmM2d3h3c3k0ZHY4eHh2ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/777Aby0ZetYE8/giphy.gif"; // Confused GIF
                 return;
             }
 
@@ -24,12 +26,15 @@
             if (items.length === 0) {
                 $scope.message = "Please enter data first";
                 $scope.messageStyle = { "color": "red" };
+                $scope.gifUrl = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTA3M2ltcmNud2RneW82dzliODQ1anZtMmM2d3h3c3k0ZHY4eHh2ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/777Aby0ZetYE8/giphy.gif"; // Confused GIF
             } else if (items.length <= 3) {
                 $scope.message = "Enjoy!";
                 $scope.messageStyle = { "color": "green" };
+                $scope.gifUrl = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWdrd3VubDFieHdiMnM0MmcxMW91eGJvazA5MHJiZ3V2YWtybXhkbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jKaFXbKyZFja0/giphy.gif"; // Happy eating GIF
             } else {
                 $scope.message = "Too much!";
                 $scope.messageStyle = { "color": "green" };
+                $scope.gifUrl = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2RzYzJ0Z2xkOHlkcnA4cGtnM3hkdnU0NjZzeHB5dnYwcmc3Mnd2MSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/12uXi1GXBibALC/giphy.gif"; // Overeating GIF
             }
         };
     }
